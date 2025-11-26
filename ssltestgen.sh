5982c7c6645a10d0df4e6fac54409039670d4b03
Tomasz 'CeDeROM' CEDRO <tomek@cedro.info>
2025-11-26 04:41:51 +0100

    ssltestgen: Add script to generate local test ssl key+crt.
    * This script genrates local testing SSL key and certificate using openssl.
    * Enter key password (used to sign crt and launch srv) and cert details.
    * Then: `bundle exec jekyll serve --ssl-cert server.crt --ssl-key server.key`.
    
    Signed-off-by: Tomasz 'CeDeROM' CEDRO <tomek@cedro.info>

---
+++ ssltestgen.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to you under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script genrates local testing SSL key and certificate using openssl.
+# Enter key password (used to sign crt and launch srv) and cert details.
+# Then: bundle exec jekyll serve --ssl-cert server.crt --ssl-key server.key
+
+set -e
+
+openssl genrsa -des3 -out server.key 1024
+openssl req -new -key server.key -out server.csr
+openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
